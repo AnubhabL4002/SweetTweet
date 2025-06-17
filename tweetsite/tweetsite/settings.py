1a17ce2c3754827fa2aa972ce4dbae442cfc1439
Anubhab Lahiri <158812807+AnubhabL4002@users.noreply.github.com>
2025-06-17 18:07:03 +0530

    My SweetTweet is stored here 2

--- tweetsite/tweetsite/settings.py
+++ tweetsite/tweetsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-nvv(mm%=^%ga%p-ez4dngsx!n*uhl8ry!j4!3fuaqgu^q+#dr^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sweettweet-ov4o.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
